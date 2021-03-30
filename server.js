2cf7a9639c83398c34ea9695c9e7816cf9910618
Curt Bridgers <bridgerspc@appstate.edu>
2021-03-30 17:49:56 -0400

    Deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
 const expensesRouter = require('./routes/api/expenses');
 
@@ -22,6 +23,16 @@ mongoose.connect(dburi, { useNewUrlParser: true, useUnifiedTopology: true }, err
 
 app.use(express.json());
 app.use('/api/expenses', expensesRouter);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.use('*', (req, res) => {
     res.status(404).send("404: Page Not Found...");
